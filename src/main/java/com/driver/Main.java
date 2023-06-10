c34764173294c542033077d36ce7c5ea54d38638
Abhay Gupta <abhaygupta82764@gmail.com>
2023-06-10 23:11:16 +0530

    code added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-  static class Product{
+  public static class Product{
     public int product(int x, int y){
       return x*y;
     }
@@ -12,10 +12,10 @@ public class Main {
       return x*y;
     }
   }
-  public static void main(String[] args){
-    Product p=new Product();
-    p.product(10,20);
-    p.product(10,20,30);
-    p.product(10.5,20.6);
+  public static void main(String[] args) {
+    Product p = new Product();
+    p.product(1, 2);
+    p.product(1, 2, 3);
+    p.product(2.5, 3.5);
   }
 }
